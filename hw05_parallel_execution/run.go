d9446d3cb4205924ae63fa54352bdee505b21317
isteshkov <denny.wise.cropp@gmail.com>
2021-05-01 15:51:16 +0300

    isnt complete

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -11,5 +11,40 @@ type Task func() error
 // Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
 func Run(tasks []Task, N int, M int) error {
 	// Place your code here
+	jobsCh := make(chan Task, len(tasks))
+	cancelCh := make(chan struct{})
+	errorsCh := make(chan error, M)
+
+	if M >= 0 {
+
+	}
+
+	go func() {
+
+	}()
+
+	go func() {
+		for i := range tasks {
+			jobsCh <- tasks[i]
+		}
+		close(jobsCh)
+	}()
+
+	for i := 0; i < N; i++ {
+		go func(cancel chan struct{}) {
+			for {
+				select {
+				case <-cancel:
+					return
+				case f := <-jobsCh:
+					err := f()
+					if err != nil {
+
+					}
+				}
+			}
+		}()
+	}
+
 	return nil
 }
