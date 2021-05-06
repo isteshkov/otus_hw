0a7bc239ce5c378325a52e63492a078c8aba672b
i.steshkov <i.steshkov@tochno.pro>
2021-05-06 10:55:38 +0300

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,49 +2,90 @@ package hw05_parallel_execution //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
 func Run(tasks []Task, N int, M int) error {
 	// Place your code here
-	jobsCh := make(chan Task, len(tasks))
-	cancelCh := make(chan struct{})
-	errorsCh := make(chan error, M)
+	doneCh := make(chan struct{})
+	tasksCh := make(chan Task)
+	errorsCh := make(chan struct{})
+	completeCh := make(chan struct{})
 
-	if M >= 0 {
-
-	}
-
-	go func() {
-
-	}()
-
-	go func() {
+	go func(done chan struct{}) {
+		defer close(tasksCh)
 		for i := range tasks {
-			jobsCh <- tasks[i]
+			select {
+			case <-done:
+				return
+			default:
+			}
+
+			select {
+			case <-done:
+				return
+			default:
+				tasksCh <- tasks[i]
+			}
 		}
-		close(jobsCh)
-	}()
+	}(doneCh)
 
 	for i := 0; i < N; i++ {
-		go func(cancel chan struct{}) {
+		go func(done <-chan struct{}, errCh chan<- struct{}, complete chan<- struct{}) {
 			for {
 				select {
-				case <-cancel:
+				case <-done:
 					return
-				case f := <-jobsCh:
-					err := f()
-					if err != nil {
+				default:
+				}
 
+				select {
+				case <-done:
+					return
+				case task := <-tasksCh:
+					if task != nil {
+						err := task()
+						if err != nil {
+							errCh <- struct{}{}
+						} else {
+							complete <- struct{}{}
+						}
 					}
 				}
 			}
-		}()
+		}(doneCh, errorsCh, completeCh)
 	}
 
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		defer wg.Done()
+		complete := 0
+		errs := 0
+		for {
+			select {
+			case <-errorsCh:
+				if M <= 0 {
+					return
+				}
+				errs++
+				if errs == M {
+					return
+				}
+			case <-completeCh:
+				complete++
+				if complete == len(tasks) {
+					return
+				}
+			}
+		}
+	}(wg)
+
+	wg.Wait()
+	close(doneCh)
 	return nil
 }
