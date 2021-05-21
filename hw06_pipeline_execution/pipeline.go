7e319aa5e393c006bb7efad385a54b412e236432
i.steshkov <i.steshkov@tochno.pro>
2021-05-21 10:49:57 +0300

    HW06 completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -10,5 +10,35 @@ type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	// Place your code here.
+
+	if len(stages) > 1 {
+		out := WrapStage(ExecutePipeline(in, done, stages[:len(stages)-1]...), done, stages[len(stages)-1])
+		if out != nil {
+			return out
+		}
+	}
+
+	if len(stages) == 1 {
+		out := WrapStage(in, done, stages[0])
+		if out != nil {
+			return out
+		}
+	}
+
 	return nil
+}
+
+func WrapStage(in In, done In, stage Stage) Out {
+	inStream := make(Bi)
+	go func() {
+		defer close(inStream)
+		for i := range in {
+			select {
+			case <-done:
+				return
+			case inStream <- i:
+			}
+		}
+	}()
+	return stage(inStream)
 }
